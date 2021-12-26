5fcceabc46604398cd54b7e3c9ed831a894f6b30
Седов Станислав <SedovSG@yandex.ru>
2021-12-27 00:26:35 +0500

    Add scripts to build an OS image using Packer

---
+++ packer/files/install_reddit.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+sleep 30 && apt-get update && apt-get install -y git
+cd /opt
+git clone -b monolith https://github.com/express42/reddit.git
+cd reddit && bundle install
+
+cd /etc/systemd/system
+cat <<EOT > ./myreddit.service
+[Unit]
+Description=Reddit Service
+After=network.target
+[Service]
+Type=simple
+ExecStart=/usr/local/bin/puma --dir /opt/reddit
+[Install]
+WantedBy=multi-user.target
+EOT
+
+chmod +x ./myreddit.service
+systemctl daemon-reload
+systemctl enable myreddit
+systemctl start myreddit
